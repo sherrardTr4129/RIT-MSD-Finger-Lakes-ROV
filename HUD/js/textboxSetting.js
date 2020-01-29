1b4c8ebe8af8f3631178b600545947f80a554a26
Trevor Sherrard <tws4129@rit.edu>
2020-01-28 20:39:24 -0500

    add toggling indicator per design review comments

---
+++ HUD/js/textboxSetting.js
@@ -0,0 +1,7 @@
+function changeStatusGreen() {
+	var elem = document.getElementById("indicator").style.backgroundColor = "green";
+}
+
+function changeStatusRed() {
+        var elem = document.getElementById("indicator").style.backgroundColor = "red";
+}
